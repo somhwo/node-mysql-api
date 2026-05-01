2a87b4d2752fc537f08834f0613161127eda5b9f
Samantha <samantha@example.com>
2026-05-01 22:11:08 +0800

    chore: set up postman and swagger.yaml

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -1,28 +1,31 @@
-import config from '../config.json';
-import mysql from 'mysql2/promise';
-import { Sequelize } from 'sequelize';
-import accountModel from '../accounts/account.model';
-import refreshTokenModel from '../accounts/refresh-token.model';
+import config from "../config.json";
+import mysql from "mysql2/promise";
+import { Sequelize } from "sequelize";
+import accountModel from "../accounts/account.model";
+import refreshTokenModel from "../accounts/refresh-token.model";
 
-const db: any = {}
+const db: any = {};
 export default db;
 
 initialize();
 
 async function initialize() {
-  const { host, port, user, password, database } = config.database;
-  const connection = await mysql.createConnection({ host, port, user, password });
+    const {host, port, user, password, database} = config.database;
+    const connection = await mysql.createConnection({host, port, user, password});
 
-  // Create DB if it doesn't exist 
-  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+    // Create DB if it doesn't exist
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
-  // Connect to DB
-  const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    // Connect to DB
+    const sequelize = new Sequelize(database, user, password, {dialect: 'mysql'});
 
-  // Init models
-  db.Account = accountModel(sequelize);
-  db.RefreshToken.belongsTo(db.Account);
+    // Init models
+    db.Account = accountModel(sequelize);
+    db.RefreshToken = refreshTokenModel(sequelize);
 
-  // Sync models with database
-  await sequelize.sync();
+    // Define relationships
+    db.Account.hasMany(db.RefreshToken, {onDelete: 'CASCADE'});
+
+    // Sync models with database
+    await sequelize.sync();
 }
